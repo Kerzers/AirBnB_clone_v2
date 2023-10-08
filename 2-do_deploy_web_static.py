e5cf69d7d870a53dbd2726903810fde5dd8af443
kerzers <ahra.ahlame@gmail.com>
2023-10-08 18:07:27 +0000

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -37,19 +37,19 @@ def do_deploy(archive_path):
     archive = archive_path.split("/")[-1]
     name = archive.split(".")[0]
 
-    for host in env.hosts:
-        try:
-            put(archive_path, '/tmp/'.format(archive))
-            run("mkdir -p /data/web_static/releases/{}/".format(name))
-            run("tar -xzf /tmp/{} -C \
-               /data/web_static/releases/{}/".format(archive, name))
-            run("rm /tmp/{}".format(archive))
-            run("mv /data/web_static/releases/{}/web_static/*\
-               /data/web_static/releases/{}/".format(name, name))
-            run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-            run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{}/ \
-                    /data/web_static/current".format(name))
-            return True
-        except Exception:
-            return False                               
+    try:
+        put(archive_path, '/tmp/{}'.format(archive))
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C \
+           /data/web_static/releases/{}/".format(archive, name))
+        run("rm /tmp/{}".format(archive))
+        run("mv /data/web_static/releases/{}/web_static/*\
+           /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ \
+                /data/web_static/current".format(name))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False                               
