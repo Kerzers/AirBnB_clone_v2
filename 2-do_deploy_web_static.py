52f55096e951387e9d38a73cb24ba0ca099f50da
kerzers <ahra.ahlame@gmail.com>
2023-10-08 17:27:38 +0000

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 # generates a .tgz archive from the contents of the web_static folder
 
 from datetime import datetime
-from fabric.api import Connection, local, env, put, run
+from fabric.api import local, env, put, run
 import os
 
 env.hosts = ['52.86.82.70', '3.85.148.240']
@@ -38,20 +38,18 @@ def do_deploy(archive_path):
     name = archive.split(".")[0]
 
     for host in env.hosts:
-        with Connection(host) as conn:
-            try:
-                conn.put(archive_path, f"/tmp/{archive}")
-                conn.run(f"mkdir -p /data/web_static/releases/{name}/")
-                conn.run(f"tar -xzf /tmp/{archive} -C \
-                        /data/web_static/releases/{name}/")
-                conn.run(f"rm /tmp/{archive}")
-                conn.run(f"mv /data/web_static/releases/{name}/web_static/*\
-                        /data/web_static/releases/{name}/")
-                conn.run(f"rm -rf /data/web_static/releases/{name}/web_static")
-                conn.run(f"rm -rf /data/web_static/current")
-                conn.run(f"ln -s /data/web_static/releases/{name}/ \
-                        /data/web_static/current")
-                return True
-            except Exception:
-                return False
-~                               
+        try:
+            put(archive_path, '/tmp/'.format(archive))
+            run("mkdir -p /data/web_static/releases/{}/".format(name))
+            run("tar -xzf /tmp/{} -C \
+               /data/web_static/releases/{}/".format(archive, name))
+            run("rm /tmp/{}".format(archive))
+            run("mv /data/web_static/releases/{}/web_static/*\
+               /data/web_static/releases/{}/".format(name, name))
+            run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/{}/ \
+                    /data/web_static/current".format(name))
+            return True
+        except Exception:
+            return False                               
